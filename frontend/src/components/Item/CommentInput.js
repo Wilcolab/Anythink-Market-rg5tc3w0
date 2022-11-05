bffe74518367ed0a9619611f818358bf54f6828b
Shreyash Singh <Rohitsingh9450@hotmail.com>
2022-11-06 03:24:28 +0530

    update

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -3,7 +3,9 @@ import agent from "../../agent";
 import { connect } from "react-redux";
 import { ADD_COMMENT } from "../../constants/actionTypes";
 
-const mapDispatchToProps = newFunction();
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (payload) => dispatch({ type: ADD_COMMENT, payload }),
+});
 
 const CommentInput = (props) => {
   const [body, setBody] = React.useState("");
@@ -14,34 +16,31 @@ const CommentInput = (props) => {
     props.onSubmit(payload);
   };
   return (
-    <form className="card comment-form" onSubmit={handleSubmit}>
-      <div className="card-block">
-        <textarea
-          className="form-control"
-          placeholder="Write a comment..."
-          value={body}
-          onChange={(ev) => setBody(ev.target.value)}
-          rows="3"
-        ></textarea>
-      </div>
-      <div className="card-footer">
-        <img
-          src={props.currentUser.image}
-          className="comment-author-img"
-          alt={props.currentUser.username}
-        />
-        <button className="btn btn-sm btn-primary" type="submit">
-          Post Comment
-        </button>
-      </div>
-    </form>
+    <form className="card comment-form m-2" onSubmit={handleSubmit}>
+    <div className="card-block">
+      <textarea
+        className="form-control"
+        placeholder="Write a comment..."
+        value={body}
+        onChange={(ev) => setBody(ev.target.value)}
+        rows="3"
+      ></textarea>
+    </div>
+    <div className="card-footer">
+      <img
+        src={this.props.currentUser.image}
+        className="user-pic mr-2"
+        alt={this.props.currentUser.username}
+      />
+      <button className="btn btn-sm btn-primary" type="submit">
+        Post Comment
+      </button>
+    </div>
+  </form>
   );
 };
 
-export default connect(() => ({}), mapDispatchToProps)(CommentInput);
-function newFunction() {
-  return (dispatch) => ({
-    onSubmit: (payload) => dispatch({ type: ADD_COMMENT, payload }),
-  });
-}
 
+
+
+export default connect(() => ({}), mapDispatchToProps)(CommentInput);
